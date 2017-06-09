82f8c5cd2933c38803f0dafec1b177901db65f00
Ian MacLeod <ian@nevir.net>
2017-06-08 21:24:00 -0700

    Filter by season

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import * as _ from 'lodash';
 import * as React from 'react';
 
 import { ItemTable } from './components';
-import { items, types } from './data';
+import { items } from './data';
+import { Item, Season } from './data/types';
 
 import './App.css';
 
+const allItems = _.values(items) as Item[];
+
 interface State {
-  items: types.Item[];
+  items: Item[];
 }
 
 export default class App extends React.Component<{}, State> {
@@ -21,14 +24,34 @@ export default class App extends React.Component<{}, State> {
   render() {
     const { items } = this.state;
 
-    return <ItemTable items={items} />;
+    return (
+      <div>
+        <select onChange={this._onSeasonChange}>
+          <option value='all'>All Seasons</option>
+          <option value={Season.Spring}>Spring</option>
+          <option value={Season.Summer}>Summer</option>
+          <option value={Season.Fall}>Fall</option>
+          <option value={Season.Winter}>Winter</option>
+        </select>
+        <ItemTable items={items} />
+      </div>
+    );
   }
 
-  _items() {
-    return _(items)
-      .values()
-      .sortBy('name')
-      .value() as types.Item[];
+  _items(season?: Season) {
+    let items = allItems;
+    if (_.isNumber(season)) {
+      items = _.filter(items, item => !item.seasons || _.includes(item.seasons, season));
+    }
+
+    return _.sortBy(items, 'name');
+  }
+
+  _onSeasonChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const { value } = event.target;
+    const season = value === 'all' ? undefined : parseInt(event.target.value, 10);
+
+    this.setState({ items: this._items(season) });
   }
 
 }
